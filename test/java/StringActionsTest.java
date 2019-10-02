54465ccf56d2b42610ee33e06d9ad49d5f4b25a1
evergood <kanzann@gmail.com>
2019-10-02 23:33:44 +0300

    v1.9

--- test/java/StringActionsTest.java
+++ test/java/StringActionsTest.java
@@ -1,25 +1,30 @@
 import defaultpackage.StringsActions;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.*;
 
 public class StringActionsTest {
+    private static StringsActions action;
+
+    @Before
+    public  void createAction(){
+        action = new StringsActions();
+    }
+
     @Test
     public void resultsShouldBeEqual() {
-        StringsActions actions = new StringsActions();
-        String result = actions.makeAnagram("tt44ews76i");
+        String result = action.makeAnagram("tt44ews76i");
         assertEquals("is44wet76t", result);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void zeroStringShouldThrowException() {
-        StringsActions action = new StringsActions();
         action.makeAnagram("");
     }
 
     @Test(expected = NullPointerException.class)
     public void nullStringShouldThrowException() {
-        StringsActions action = new StringsActions();
         action.makeAnagram(null);
     }
 }
