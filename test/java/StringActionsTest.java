bb19a3e35fb8406ec2e8c13861dc606f69ccc01f
evergood <kanzann@gmail.com>
2019-10-12 22:29:15 +0300

    v2.4

--- test/java/StringActionsTest.java
+++ test/java/StringActionsTest.java
@@ -12,44 +12,45 @@ public class StringActionsTest {
 
     @Test
     void makeAnagramShouldReturnAnagramForOneWordWithNumbersAndNoSymbolsArgument(){
-        String actual = anagramaMaker.makeAnagram("tt44ews76i");
-        assertEquals("is44wet76t", actual);
+        final String actual = anagramaMaker.makeAnagram("tt44ews76i");
+        final String expected = "is44wet76t";
+        assertEquals(expected, actual);
     }
 
     @ParameterizedTest
     @CsvSource({"a,a","aaa,aaa","AAAAA,AAAAA","AAAaaaA,AaaaAAA"})
     void makeAnagramShouldReturnAnagramForOneWordOneLetterArgumentInDifferentCases(String input,String expected){
-        assertEquals(anagramaMaker.makeAnagram(input),expected);
+        assertEquals(anagramaMaker.makeAnagram(input), expected);
     }
 
     @ParameterizedTest
     @CsvSource({"abc def,cba fed","sugh iuhs,hgus shui ","rt ew,tr we"})
     void makeAnagramShouldReturnAnagramForTwoWordOnlyLettersArgument(String input, String expected){
-        assertEquals(anagramaMaker.makeAnagram(input),expected);
+        assertEquals(anagramaMaker.makeAnagram(input), expected);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1","1244235","(&^(%@!%<><^{:","876*%!76&%@!((^&^&",})
     void makeAnagramShouldKeepOneWordNonletterArgumentsTheSame(String input){
-        assertEquals(anagramaMaker.makeAnagram(input),input);
+        assertEquals(anagramaMaker.makeAnagram(input), input);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"12442 35","(&^(%@!% <><^{:","876*%!7 6&%@!((^&^&",})
     void makeAnagramShouldKeepTwoWordsNonletterArgumentsTheSame(String input){
-        assertEquals(anagramaMaker.makeAnagram(input),input);
+        assertEquals(anagramaMaker.makeAnagram(input), input);
     }
 
     @ParameterizedTest
     @CsvSource({"ab3c d1ef,cb3a f1ed","su@gh i!!uhs,hg@us s!!hui ","rt# %ew,tr# %we"})
     void makeAnagramShouldReturnAnagramForTwoWordLetterAndNonletterArgument(String input, String expected){
-        assertEquals(anagramaMaker.makeAnagram(input),expected);
+        assertEquals(anagramaMaker.makeAnagram(input), expected);
     }
 
     @ParameterizedTest
     @CsvSource({"ab3c d1ef i55r,cb3a f1ed r55i","su@gh i!!uhs s**re,hg@us s!!hui e**rs","rt# %ew !34,tr# %we !34"})
     void makeAnagramShouldReturnAnagramForSeveralWordLetterAndNonletterArgument(String input, String expected){
-        assertEquals(anagramaMaker.makeAnagram(input),expected);
+        assertEquals(anagramaMaker.makeAnagram(input), expected);
     }
 
     @ParameterizedTest
