b9b3b4ad9a9c878ecc62796e54b6d62909166f37
evergood <kanzann@gmail.com>
2019-10-18 01:15:41 +0300

    v3

--- src/main/java/com/foxminded/homework/IntegerDivider.java
+++ src/main/java/com/foxminded/homework/IntegerDivider.java
@@ -1,10 +1,21 @@
 package com.foxminded.homework;
 
 public class IntegerDivider {
+
+    public void print(char[][] field){
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public void divide(int divident, int divisor) {
         String dividentString = String.valueOf(divident);
         char divisorChar = Character.forDigit(divisor, 10);
-        char[][] divisionField = new char[dividentString.length() * 2 + 2][dividentString.length() * 3 + 1];
+        //char[][] divisionField = new char[dividentString.length() * 2 + 2][dividentString.length() * 3 + 1];
+        char[][] divisionField = new char[dividentString.length() * 2 + 4][dividentString.length() * 3 + 1];
         divisionField[0][0] = '_';
         for (int i = 1; i <= dividentString.length(); i++) {
             divisionField[0][i] = dividentString.charAt(i - 1);
@@ -17,30 +28,32 @@ public class IntegerDivider {
         int m = 0;
         int k = 1;
 
-        for (int i = 1; i < dividentString.length(); i++) {
-            k = i;
+        while (k <= dividentString.length()){
             if (middleResult(divisionField, m, k) > divisor) {
-                k = i;
-                divisionField[++m][k] = divisorChar;
+                divisionField[++m][k] = Character.forDigit(middleResult(divisionField, m-1, k)/divisor*divisor%10, 10);
+                if (middleResult(divisionField, m-1, k)/divisor*divisor/10 > 0 ){
+                    divisionField[m][k-1] = Character.forDigit(middleResult(divisionField, m-1, k)/divisor*divisor/10, 10);
+                }
                 divisionField[++m][k] = '-';
-                divisionField[++m][k] = Character.forDigit(Character.getNumericValue(divisionField[m - 3][k]) % divisor, 10);
-                divisionField[m][++k] = divisionField[0][k];
+                divisionField[++m][k] = Character.forDigit(middleResult(divisionField, m-3, k) - middleResult(divisionField, m-2, k), 10);
+               if (k != dividentString.length()){
+                   divisionField[m][++k] = divisionField[0][k];
+               }
+               else k++;
             }
         }
 
-
-        for (int i = 0; i < divisionField.length; i++) {
-            for (int j = 0; j < divisionField[i].length; j++) {
-                System.out.print(divisionField[i][j]);
-            }
-            System.out.println();
-        }
+        print(divisionField);
     }
 
     private int middleResult(char[][] field, int row, int column) {
+        int d;
         if (Character.isDigit(field[row][column-1])){
-            return Character.getNumericValue(field[row][column-1]) * 10 + Character.getNumericValue(field[row][column]);
+            d =  Character.getNumericValue(field[row][column-1]) * 10 + Character.getNumericValue(field[row][column]);
         }
-        else return Character.getNumericValue(field[row][column]);
+        else{
+            d = Character.getNumericValue(field[row][column]);
+        }
+        return d;
     }
 }
